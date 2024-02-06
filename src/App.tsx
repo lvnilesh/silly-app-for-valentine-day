7c41edcfca1b30140cb952d2a3eb9b797502f2ab
LV Nilesh <nilesh@cloudgeni.us>
2024-02-06 08:50:30 -0800

    Update phrases.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,31 @@
-"use client";
 import { useState } from "react";
 import phrases from "./phrases.json";
 
 export default function Page() {
   const [noCount, setNoCount] = useState(0);
   const [yesPressed, setYesPressed] = useState(false);
+  const [currentImageIndex, setCurrentImageIndex] = useState(0); // State to track current image index
+  const images = [
+    "https://cdn.memegenerator.es/imagenes/memes/full/3/13/3130348.jpg",
+    "https://i.imgflip.com/1ovjew.jpg",
+    "https://th.bing.com/th/id/OIP.kYbrTCtOureAk6px8MIxtQHaHa?rs=1&pid=ImgDetMain",
+    "https://www.meme-arsenal.com/memes/72dd941ddbed72b3d7765a8e64becb11.jpg",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSB8NLk90Q3_8oNci-56Wy1IAKgXf6WhICACw&usqp=CAU",
+    "https://media.tenor.com/Yrj3F_-AGKkAAAAi/cartoon-cute.gif",
+    "https://media1.tenor.com/m/K1VyJCb1im0AAAAC/kid-crying.gif",
+    "https://media1.tenor.com/m/CyJzmWAjPTEAAAAC/cat-no.gif",
+    "https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExN3ZvcmM2dWtjcjl1MXBjYTV4Y3oybnM4ZnhjZGRsMW1jM3phNXR0ZiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/JMe4NTbuueHnZdZlTh/giphy.gif",
+    "https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExM2oxOHU5bmVhemd5bGNjbHhqbmhyNjk5a2x2Z2JlY2M2OGg3YmR1YSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/I1nwVpCaB4k36/giphy.gif",
+    "https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExdHE0ZjkxMnZscTRpN3hzOTczeXV5bDY0Z3RuNnBnMzI5MW9xeG1sYSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/8vUEXZA2me7vnuUvrs/giphy.gif",
+    // Add more image URLs as needed
+  ];
   const yesButtonSize = noCount * 20 + 16;
 
   const handleNoClick = () => {
     setNoCount(noCount + 1);
+    setYesPressed(false); // Resetting yesPressed state
+    // Increment current image index and reset to 0 if it exceeds the number of images
+    setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
   };
 
   const getNoButtonText = () => {
@@ -21,9 +38,9 @@ export default function Page() {
         display: "flex",
         alignItems: "center",
         justifyContent: "center",
+        flexDirection: "column", // Center content vertically
         height: "100vh",
       }}
-      className="flex flex-col items-center justify-center h-screen -mt-16"
     >
       {yesPressed ? (
         <>
@@ -61,6 +78,14 @@ export default function Page() {
               {noCount === 0 ? "Yes" : getNoButtonText()}
             </button>
           </div>
+          {noCount > 0 && ( // Render image only if noCount is greater than 0
+            <div>
+              <img
+                src={images[currentImageIndex]} // Use current image URL based on currentImageIndex
+                alt="Meme Images."
+              />
+            </div>
+          )}
         </>
       )}
     </div>
